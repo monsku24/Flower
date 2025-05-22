48bbac15b6bbaaf78ba74b37e43362af555db05e
monsku24 <159902713+monsku24@users.noreply.github.com>
2025-05-22 21:28:43 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `I Have Something`.split('')
+const text = `HBDD NABB SELAMAT MENUA NABB MENJADI 19 THN`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
