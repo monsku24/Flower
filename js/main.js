be96ac68d63b19d691f7828ad83bc228a8ce55c2
monsku24 <159902713+monsku24@users.noreply.github.com>
2025-05-22 21:36:01 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ onload = () => {
   const c = setTimeout(() => {
     document.body.classList.remove("not-loaded");
 
-    const titles = ('HBDD NABB SELAMAT MENUA NABB MENJADI 19 THN').split('')
+    const titles = ('HBDD BUAT PART 2 NYA NABB SELAMAT MENUWA').split('')
     const titleElement = document.getElementById('title');
     let index = 0;
 
